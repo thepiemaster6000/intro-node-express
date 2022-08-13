abe6fe71a92150d0a7d8972e0231a837da284727
jtg91389 <jgear91389@gmail.com>
2022-08-13 12:35:40 -0500

    codealong for 8-13 intro to express

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+// app.get('/', function(req, res) {
+//     res.send('Hello, World!');
+// });
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname+'/views/index.html');
+});
+
+app.get('/about', function(req, res) {
+    res.sendFile(__dirname+'/views/about.html');
+});
+
+app.get('/blog', function(req, res) {
+    res.sendFile(__dirname+'/views/blog.html');
+});
+
+app.get('/getMessage', function(req, res) {
+    res.status(200).send('this is a message from our backend service!');
+});
+
+app.get('/getMessageJson', function(req, res) {
+    res.status(200).send({ message: 'this is a json response from our backend service!'});
+});
+
+app.listen(8000);
